c9181394bd0c4fdb0eeeea1cb700e3357985b745
dnsamw <dnsam.mit@gmail.com>
2023-08-25 09:54:46 +0530

    progress bar circles fix

--- subscription.js
+++ subscription.js
@@ -1,74 +1,51 @@
 // const prev = document.getElementById("prev");
 // const next = document.getElementById("next");
-// const progress = document.getElementById("progress");
-// const circles = document.querySelectorAll(".circle");
-// const slides = document.querySelectorAll(".slide");
-
-// console.log(slides);
-
-// let currentActive = 1;
-
-// next.addEventListener("click", () => {
-//   currentActive++;
-
-//   if (currentActive > circles.length) {
-//     currentActive = circles.length;
-//   }
-
-//   update();
-// });
-
-// prev.addEventListener("click", () => {
-//   currentActive--;
-
-//   // prevents currentActive from going below 1
-//   if (currentActive < 1) {
-//     currentActive = 1;
-//   }
-
-//   update();
-// });
-
-// function update() {
-//   circles.forEach((circle, idx) => {
-//     if (idx < currentActive) {
-//       circle.classList.add("active");
-//     } else {
-//       circle.classList.remove("active");
-//     }
-//   });
-
-//   slides.forEach((slide, idx) => {
-//     if (idx < currentActive) {
-//       slide.classList.add("active");
-//     } else {
-//       slide.classList.remove("active");
-//     }
-//   });
-// }
-
-// const actives = document.querySelectorAll(".active");
-
-// progress.style.width =
-//   ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
-
-// disables prev when you can't go back further, disables next when there are no more steps
-//   if (currentActive === 1) {
-//     prev.disabled = true;
-//   } else if (currentActive === circles.length) {
-//     next.disabled = true;
-//   } else {
-//     prev.disabled = false;
-//     next.disabled = false;
-//   }
-// }
 
 document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".slide");
   const prev = document.getElementById("prev");
   const next = document.getElementById("next");
+  const progress = document.getElementById("progress");
+  const circles = document.querySelectorAll(".circle");
 
   let currentSlideIndex = 0;
+  let currentActive = 1;
+
+  function update() {
+    circles.forEach((circle, idx) => {
+      if (idx < currentActive) {
+        circle.classList.add("active");
+      } else {
+        circle.classList.remove("active");
+      }
+    });
+
+    slides.forEach((slide, idx) => {
+      if (idx < currentActive) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  }
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  console.log(progress.style.width, actives.length);
+  console.log((actives.length - 1) / (circles.length - 1));
+
+  // disables prev when you can't go back further, disables next when there are no more steps
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 
   function showSlide(index) {
     slides.forEach((slide, i) => {
@@ -94,10 +71,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   prev.addEventListener("click", () => {
     navigateSlide(-1);
+    currentActive--;
+
+    // prevents currentActive from going below 1
+    if (currentActive < 1) {
+      currentActive = 1;
+    }
+
+    update();
   });
 
   next.addEventListener("click", () => {
     navigateSlide(1);
+    currentActive++;
+
+    if (currentActive > circles.length) {
+      currentActive = circles.length;
+    }
+
+    update();
   });
 
   // Show the initial slide
